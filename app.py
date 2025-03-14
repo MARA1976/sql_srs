f8fbefbe99b4d413867bc9a58352bfe5f836215b
ahlem <oueslatiahlem76@gmail.com>
2025-03-14 22:22:54 +0100

    updated

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ df = pd.DataFrame(data)
 
 # Onglet 1 - Saisie de requête SQL
 with tab1:
-    sql_query = st.text_area(label="Entrez votre input")
+    sql_query = st.text_area(label="Enter your input")
 
     if sql_query.strip():  # Si une requête est saisie
         try:
             result = duckdb.query(sql_query).df()
-            st.write(f"Vous avez entré la query suivante : {sql_query}")
+            st.write(f"You have entered the following query: {sql_query}")
             st.dataframe(result)
         except Exception as e:
             st.error(f"Erreur lors de l'exécution de la requête : {e}")
